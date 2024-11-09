e0a2936459568c389a8d45cbf6554c95ffc960ff
waleed-abuqalben <waleed.abuqalben@gmail.com>
2024-11-10 00:58:30 +0300

    set up backend and frontend using Spring init & Vite

---
+++ backend/src/test/java/com/linkedin/linkedin/LinkedinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linkedin.linkedin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkedinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
